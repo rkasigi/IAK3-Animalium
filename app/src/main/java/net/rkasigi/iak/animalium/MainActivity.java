26e0b4a04fc62ab2c68d33bd0945a5a5f07e4c40
Rendi Kasigi <rkasigi@gmail.com>
2017-05-07 15:27:51 +0700

    add field loginName on FrontActivity

--- app/src/main/java/net/rkasigi/iak/animalium/MainActivity.java
+++ app/src/main/java/net/rkasigi/iak/animalium/MainActivity.java
@@ -2,12 +2,18 @@ package net.rkasigi.iak.animalium;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String loginName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginName = getIntent().getStringExtra("loginName");
+
+        Toast.makeText(this, loginName, Toast.LENGTH_LONG).show();
     }
 }
