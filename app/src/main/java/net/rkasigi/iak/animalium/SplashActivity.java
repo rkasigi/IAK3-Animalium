9244004511d2aa9af7b279667583d97b6a71256d
Rendi Kasigi <rkasigi@gmail.com>
2017-05-07 14:50:01 +0700

    add splashscreen

---
+++ app/src/main/java/net/rkasigi/iak/animalium/SplashActivity.java
@@ -0,0 +1,27 @@
+package net.rkasigi.iak.animalium;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Intent i = new Intent(this, FrontActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+
+            }
+        }, 2000);
+
+    }
+}
